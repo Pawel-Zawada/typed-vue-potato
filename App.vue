761ebe182c9414e34bc1f5ac5608ccff718846df
Ian den Hartog <iandenhartog@hotmail.com>
2019-08-20 14:50:12 +0200

    Add start of Vue with TS for admin

---
+++ App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>{{text}}</h1>
+        <button @click="save">hallo</button>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    data() {
+        return {
+            text: 'hallo'
+        }
+    },
+    methods: {
+        save() {
+            alert("saved");
+        }
+    }
+}
+</script>
