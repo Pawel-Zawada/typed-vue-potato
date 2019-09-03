cc52fa9d25b04774f9fc188e5b178ccddf69664b
Ian den Hartog <iandenhartog@hotmail.com>
2019-09-03 09:12:11 +0200

    Fix page var on categories api

--- api/categories.ts
+++ api/categories.ts
@@ -5,15 +5,16 @@ const baseurl = '/admin/api/categories';
 export const getCategories: API.GetDataFunction<
   Entities.Categories[],
   true
-> = async (page?: number, sort?: DataTable.SortParameter[]) => {
+> = async (page = 1, sort?: DataTable.SortParameter[]) => {
   const sortingString = '';
   if (sort) {
     sort.map(
       ({ property, direction }) => `&sort=${property}&direction=${direction}`
     );
   }
+
   const response: {
     data: API.DefaultResponse<Entities.Categories[], true>;
-  } = await axios.get(`${baseurl}${page && `?page=${page}`}${sortingString}`);
+  } = await axios.get(`${baseurl}${`?page=${page}`}${sortingString}`);
   return response.data;
 };
